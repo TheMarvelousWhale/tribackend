5a0eeab6b03ae8d919d9ea9d1840d48e5085229f
Robin <weiluobin@hotmail.com>
2022-09-10 22:29:44 +0800

    big lmao robin r u arguing w ur gf

--- download_models_and_checkpoints.sh
+++ download_models_and_checkpoints.sh
@@ -1,10 +1,18 @@
 ###
+mkdir previous_results
 git clone git@github.com:TheMarvelousWhale/lightweight-human-pose-estimation.pytorch.git pose-estimation
-git clone git clone git@github.com:TheMarvelousWhale/pifuhd.git pifuhd
-wget https://download.01.org/opencv/openvino_training_extensions/models/human_pose_estimation/checkpoint_iter_370000.pth pose-estimation/
+git clone git@github.com:TheMarvelousWhale/pifuhd.git pifuhd
+mkdir -p backend/static/uploads
+wget https://download.01.org/opencv/openvino_training_extensions/models/human_pose_estimation/checkpoint_iter_370000.pth 
+mv checkpoint_iter_370000.pth pose-estimation
+
+
 mkdir -p pifuhd/{checkpoints,sample_images}
-wget "https://dl.fbaipublicfiles.com/pifuhd/checkpoints/pifuhd.pt" pifuhd/checkpoints/pifuhd.pt
+wget "https://dl.fbaipublicfiles.com/pifuhd/checkpoints/pifuhd.pt" pifuhd.pt
+mv pifuhd.pt pifuhd/checkpoints/
 
 git clone git@github.com:TheMarvelousWhale/pix2surf.git
 #download blender on https://download.blender.org/release/Blender2.79/
 mkdir pix2surf/{output,video}
+cd pix2surf
+bash scripts/prepare_data.sh
